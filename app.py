6df7bbdcc21ea4fb5080ca0411dc3996f3fb43ff
lfkopp <lfkopp@gmail.com>
2021-05-14 15:00:46 -0300

    controle

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app.secret_key = "meuSegred0"
 def home():
     return render_template('main.html')
 
+@app.route("/graph2")
+def graph2():
+    return render_template('graph2.html')
+
 if __name__ == "__main__":
     app.run(debug=True, threaded=True, port=5000)
