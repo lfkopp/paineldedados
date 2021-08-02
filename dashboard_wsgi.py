4ad72dfba2986da3fdf9c02decbd6c60f462bf7d
lfkopp <nupec@nupec.c.vms-coppead.internal>
2021-08-02 14:14:05 +0000

    cadeado

--- dashboard_wsgi.py
+++ dashboard_wsgi.py
@@ -1,8 +1,10 @@
+
 import sys
 sys.path.append('./app')
 
 from app.dashboard import app
 from app.config import server
 
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host='0.0.0.0', port=5000)
