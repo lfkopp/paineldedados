40cae4fdb554fd90a5d81dbc4a82acdef9b22614
lfkopp <lfkopp@gmail.com>
2022-09-29 16:01:46 -0300

    app

--- dashboard_wsgi.py
+++ dashboard_wsgi.py
@@ -1,10 +1,4 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def inicial():
-    return render_template('index.html')
+from app import app
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
